1efb8a3b3ec74322a69d3e6c62b21a26226c4c86
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-14 15:33:30 +0600

    Btn

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
+type Btn = {
+    handleClick: () => void,
+    children: ReactNode
+}
 
-export default function Button({ handleClick, children }: { handleClick: () => void, children: ReactNode }) {
+export default function Button({ handleClick, children }: Btn) {
     return (
         <button onClick={handleClick}>{children}</button>
     )
