cf6e75c2d4a832be6f886bdbc68a0710db2b5e8c
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-14 14:37:30 +0600

    Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Greet from './components/Greet'
+import Heading from './components/Heading'
 import PersonList from './components/PersonList'
 import StringLiterals from './components/StringLiterals'
 import TypedProps from './components/TypedProps'
@@ -26,10 +27,13 @@ function App() {
 
   return (
     <div>
+      <Heading>
+        Heading
+      </Heading>
       <Greet message={"hi there"} name="Shourov" />
       <TypedProps message={"hi there"} name="Shourov" person={person} />
       <PersonList personList={personList} person={person} />
-      <StringLiterals status="loading" />
+      <StringLiterals status="failed" />
     </div>
   )
 }
---
+++ src/components/Heading.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export default function Heading({ children }: { children: ReactNode }) {
+    return (
+        <h1>{children}</h1>
+    )
+}
