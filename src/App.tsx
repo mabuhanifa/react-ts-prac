17cd6cb2df1b61b528ed10045748bdc5b3431143
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-14 13:00:11 +0600

    PersonList

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Greet from './components/Greet'
+import PersonList from './components/PersonList'
 import TypedProps from './components/TypedProps'
 
 function App() {
@@ -7,11 +8,26 @@ function App() {
     name: "Shourov",
     age: 26
   }
+  const personList = [
+    {
+      first: "Abdul",
+      last: "Mia"
+    },
+    {
+      first: "Abul",
+      last: "Kashem"
+    },
+    {
+      first: "Rahim",
+      last: "Khan"
+    }
+  ]
 
   return (
     <div>
       <Greet message={"hi there"} name="Shourov" />
       <TypedProps message={"hi there"} name="Shourov" person={person} />
+      <PersonList {...personList} />
     </div>
   )
 }
