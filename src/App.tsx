16f801da02175db8d5cfbacd30061ddd7374e94c
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-14 12:28:06 +0600

    person

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import Greet from './components/Greet'
 import TypedProps from './components/TypedProps'
 
 function App() {
-
+  const person = {
+    name: "Shourov",
+    age: 26
+  }
 
   return (
     <div>
       <Greet message={"hi there"} name="Shourov" />
-      <TypedProps message={"hi there"} name="Shourov" />
+      <TypedProps message={"hi there"} name="Shourov" person={person} />
     </div>
   )
 }
--- src/components/TypedProps.tsx
+++ src/components/TypedProps.tsx
@@ -1,12 +1,19 @@
 type Props = {
     message: string,
     name: string,
+    person: {
+        name: string;
+        age: number;
+    }
 }
-export default function TypedProps({ message, name }: Props) {
+export default function TypedProps({ message, name, person }: Props) {
     return (
         <div>
             <div>
                 {message} {name}
+            </div>
+            <div>
+                {person.name} age: {person.age}
             </div>
         </div>
     )
