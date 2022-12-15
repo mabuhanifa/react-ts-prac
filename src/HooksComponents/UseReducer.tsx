53e8d6feb7088c66238166a11e22ab3b4a5d6dd4
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-15 13:00:22 +0600

    useReducer

--- src/HooksComponents/UseReducer.tsx
+++ src/HooksComponents/UseReducer.tsx
@@ -1,5 +1,30 @@
+import { useReducer } from "react";
+type Action = { type: "ADD" | "REMOVE", payload: number };
+
+type Product = {
+    name: string,
+    id: number
+}
+type State = {
+    counter: number;
+    // products: Product[];
+    // cart: Product[];
+}
+const initialState = {
+    counter: 0,
+    // products: [],
+    // cart: [],
+}
+
+
+function stateReducer(state: State, action: Action) {
+
+}
+
 
 export default function UseReducer() {
+    const [state, dispatch] = useReducer(stateReducer, initialState)
+
     return (
         <div>UseReducer</div>
     )
