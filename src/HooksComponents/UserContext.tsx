23f583954a1b70304255bcc2179c20de20f9bc6c
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-16 12:04:04 +0600

    UserContextProvider

--- src/HooksComponents/UserContext.tsx
+++ src/HooksComponents/UserContext.tsx
@@ -1,4 +1,16 @@
+import { createContext, ReactNode, useState } from "react";
+
 export type AuthUser = {
     name: string;
     email: string;
+}
+
+export const UserContext = createContext(null)
+
+export const UserContextProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<null | AuthUser>(null);
+    return <UserContext.Provider value={ }>
+        {children}
+    </UserContext.Provider>
+
 }
