be575e18dc0bd40cbecd405ecf971416cf2c887c
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-16 12:21:55 +0600

    <UserContextProvider>

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import UseContext from './HooksComponents/UseContext'
+import { UserContextProvider } from './HooksComponents/UserContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <UseContext>
-      <App />
-    </UseContext>
+    <UserContextProvider>
+      <UseContext>
+        <App />
+      </UseContext>
+    </UserContextProvider>
   </React.StrictMode>
 )
