eefd60f50e37c4c1f46765dc44fd2085cbecc9c3
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-16 11:56:30 +0600

    User

---
+++ src/HooksComponents/User.tsx
@@ -0,0 +1,17 @@
+
+export default function User() {
+    const handleLogIn = () => { }
+    const handleLogOut = () => { }
+    return (
+        <div>
+            <div>
+                <button onClick={handleLogIn}>Log In</button>
+                <button onClick={handleLogOut}>Log Out</button>
+            </div>
+            <div>
+                <h2>User Name is</h2>
+                <h2>User Name is</h2>
+            </div>
+        </div>
+    )
+}
